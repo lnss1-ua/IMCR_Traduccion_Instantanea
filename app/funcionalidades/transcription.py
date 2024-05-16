30ed7bc4d6294bd6d739c447bcff563584d45ca9
Gonog <smp105@alu.ua.es>
2024-05-16 22:44:07 +0200

    fix transcription

--- app/funcionalidades/transcription.py
+++ app/funcionalidades/transcription.py
@@ -1,8 +1,7 @@
 # transcription.py
 from flask import request, jsonify
-import numpy as np
-import librosa
 import whisper
+import os
 from .translation import translate_text
 from .audio import text_to_audio
 
@@ -11,12 +10,14 @@ def transcribe_audio():
     audio_file = request.files['audio']
     output_format = request.form['output_format']
     output_lang = request.form['output_lang']
-    # Read the audio file into a NumPy array
-    audio_np, sr = librosa.load(audio_file, sr=None)
     # Load the Whisper model
     model = whisper.load_model("base") #large
+    audio_file.seek(0)
+    temp_path = f"./{audio_file.filename}"
+    audio_file.save(temp_path)
     # Transcribe the audio
-    result = model.transcribe(audio_np)
+    result = model.transcribe(temp_path)
+    os.remove(temp_path)
     # Extract the transcribed text from the result
     transcribed_text = result['text']
     if result['language'] != output_lang:
